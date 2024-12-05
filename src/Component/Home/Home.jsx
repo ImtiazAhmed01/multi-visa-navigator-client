897a32966da1e15069d33148f6576902fc1f210b
ImtiazAhmed01 <imtiazahm21@gmail.com>
2024-12-05 01:12:14 -0700

    sliders have been added in the home page component

--- src/Component/Home/Home.jsx
+++ src/Component/Home/Home.jsx
@@ -1,10 +1,56 @@
 import React from 'react';
+import slider1Image from "../../../src/assets/flat-travel-background-tourism-visa-260nw-279542630.jpg"
+import slider2Image from "../../../src/assets/analysis-data-process-target-business-260nw-503682529.jpg"
+import slider3Image from "../../../src/assets/man-using-virtual-touch-screen-260nw-2336775113.jpg"
+import slider4Image from "../../../src/assets/visa-application-approved-stamp-passport-260nw-2214866597.jpg"
 
 const Home = () => {
     return (
-        <div>
-            <h1>This is Home</h1>
+        <div className='px-32'>
+            <div className="carousel w-full">
+                <div id="slide1" className="carousel-item relative w-full">
+                    <img
+                        src={slider1Image}
+                        className="w-full h-[500px] object-contain" // Adjusted height and object-fit
+                    />
+                    <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+                        <a href="#slide4" className="btn btn-circle">❮</a>
+                        <a href="#slide2" className="btn btn-circle">❯</a>
+                    </div>
+                </div>
+                <div id="slide2" className="carousel-item relative w-full">
+                    <img
+                        src={slider2Image}
+                        className="w-full h-[500px] object-contain" // Maintains image visibility without cropping
+                    />
+                    <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+                        <a href="#slide1" className="btn btn-circle">❮</a>
+                        <a href="#slide3" className="btn btn-circle">❯</a>
+                    </div>
+                </div>
+                <div id="slide3" className="carousel-item relative w-full">
+                    <img
+                        src={slider3Image}
+                        className="w-full h-[500px] object-contain" // Adjusted height and object-fit
+                    />
+                    <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+                        <a href="#slide2" className="btn btn-circle">❮</a>
+                        <a href="#slide4" className="btn btn-circle">❯</a>
+                    </div>
+                </div>
+                <div id="slide4" className="carousel-item relative w-full">
+                    <img
+                        src={slider4Image}
+                        className="w-full h-[500px] object-contain" // Adjusted height and object-fit
+                    />
+                    <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+                        <a href="#slide3" className="btn btn-circle">❮</a>
+                        <a href="#slide1" className="btn btn-circle">❯</a>
+                    </div>
+                </div>
+            </div>
         </div>
+
     );
 };
 
